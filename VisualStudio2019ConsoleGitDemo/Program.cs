64aaf6c2aa8be2f2d710f3a807c413e99a3e4df5
sanjay-pardeshi <pardeshisanjay18.com>
2020-07-01 23:40:37 +0530

    New Line Added

--- VisualStudio2019ConsoleGitDemo/Program.cs
+++ VisualStudio2019ConsoleGitDemo/Program.cs
@@ -7,6 +7,8 @@ namespace VisualStudio2019ConsoleGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New Changes Here");
         }
     }
 }
+
